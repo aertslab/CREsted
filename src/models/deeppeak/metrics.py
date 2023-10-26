72f8219eb1b0ad3b469aa5a86c015d05562abeb8
lukasmahieu <lukas.mahieu@kuleuven.be>
2023-10-26 16:17:49 +0200

    Model architecture and metrics

---
+++ src/models/deeppeak/metrics.py
@@ -0,0 +1,63 @@
+"""Custom metrics used in DeepPeak training."""
+
+import tensorflow as tf
+
+
+def get_lr_metric(optimizer):
+    """Returns a function that gets the current learning rate from optimizer.
+    Useful for logging the current learning rate during training.
+    """
+
+    def lr(y_true, y_pred):
+        return optimizer._decayed_lr(
+            tf.float32
+        )  # I use ._decayed_lr method instead of .lr
+
+    return lr
+
+
+class PearsonCorrelation(tf.keras.metrics.Metric):
+    def __init__(self, name="pearson_correlation", **kwargs):
+        super(PearsonCorrelation, self).__init__(name=name, **kwargs)
+        self.y_true_sum = self.add_weight(name="y_true_sum", initializer="zeros")
+        self.y_pred_sum = self.add_weight(name="y_pred_sum", initializer="zeros")
+        self.y_true_squared_sum = self.add_weight(
+            name="y_true_squared_sum", initializer="zeros"
+        )
+        self.y_pred_squared_sum = self.add_weight(
+            name="y_pred_squared_sum", initializer="zeros"
+        )
+        self.y_true_y_pred_sum = self.add_weight(
+            name="y_true_y_pred_sum", initializer="zeros"
+        )
+        self.count = self.add_weight(name="count", initializer="zeros")
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_true = tf.cast(y_true, tf.float32)
+        y_pred = tf.cast(y_pred, tf.float32)
+
+        self.y_true_sum.assign_add(tf.reduce_sum(y_true))
+        self.y_pred_sum.assign_add(tf.reduce_sum(y_pred))
+        self.y_true_squared_sum.assign_add(tf.reduce_sum(tf.square(y_true)))
+        self.y_pred_squared_sum.assign_add(tf.reduce_sum(tf.square(y_pred)))
+        self.y_true_y_pred_sum.assign_add(tf.reduce_sum(y_true * y_pred))
+        self.count.assign_add(tf.cast(tf.size(y_true), tf.float32))
+
+    def result(self):
+        numerator = (
+            self.count * self.y_true_y_pred_sum - self.y_true_sum * self.y_pred_sum
+        )
+        denominator = tf.sqrt(
+            (self.count * self.y_true_squared_sum - tf.square(self.y_true_sum))
+            * (self.count * self.y_pred_squared_sum - tf.square(self.y_pred_sum))
+        )
+
+        return numerator / (denominator + tf.keras.backend.epsilon())
+
+    def reset_states(self):
+        self.y_true_sum.assign(0.0)
+        self.y_pred_sum.assign(0.0)
+        self.y_true_squared_sum.assign(0.0)
+        self.y_pred_squared_sum.assign(0.0)
+        self.y_true_y_pred_sum.assign(0.0)
+        self.count.assign(0.0)
---
+++ src/models/deeppeak/model.py
@@ -0,0 +1,153 @@
+"""DeepPeak model definition."""
+
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+
+
+class ConvBlock(layers.Layer):
+    """Convolution block with optional residual connection."""
+
+    def __init__(
+        self,
+        filters,
+        kernel_size,
+        pool_size: int = 2,
+        activation: str = "relu",
+        l2: float = 1e-5,
+        dropout: float = 0.25,
+        res: bool = False,
+    ):
+        super().__init__()
+
+        # configs
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.pool_size = pool_size
+        self.activation = activation
+        self.l2 = l2
+        self.dropout = dropout
+        self.res = res
+
+        # layers
+        self.conv = layers.Conv1D(
+            filters=self.filters,
+            kernel_size=self.kernel_size,
+            strides=1,
+            activation=None,
+            use_bias=False,
+            padding="same",
+            kernel_initializer="he_normal",
+            kernel_regularizer=tf.keras.regularizers.l2(self.l2),
+        )
+        self.conv_res = layers.Conv1D(
+            filters=self.filters,
+            kernel_size=1,
+            strides=1,
+            activation=None,
+            use_bias=False,
+            padding="same",
+            kernel_initializer="he_normal",
+            kernel_regularizer=tf.keras.regularizers.l2(self.l2),
+        )
+        self.bn = layers.BatchNormalization(momentum=0.9, gamma_initializer="ones")
+        self.activation_layer = layers.Activation(self.activation)
+        self.maxpool = layers.MaxPool1D(pool_size=self.pool_size, padding="same")
+        self.dropout_layer = layers.Dropout(self.dropout)
+
+    def call(self, inputs):
+        if self.res:
+            residual = inputs
+        x = self.conv(inputs)
+        x = self.bn(x)
+        x = self.activation_layer(x)
+
+        if self.res:
+            if self.filters != residual.shape[-1]:
+                residual = self.conv_res(residual)
+            x = tf.keras.layers.add([x, residual])
+
+        if self.pool_size > 1 and inputs.shape[1] > self.kernel_size:
+            x = self.maxpool(x)
+        if self.dropout > 0:
+            x = self.dropout_layer(x)
+        return x
+
+
+class DeepPeak(tf.keras.Model):
+    def __init__(self, num_classes: int, config: dict):
+        super().__init__()
+
+        self.config = config
+
+        self.num_classes = num_classes
+
+        self.filter_size = self.config["filter_size"]
+        self.num_filters = self.config["num_filters"]
+        self.pool_size = self.config["pool_size"]
+        self.activation = self.config["activation"]
+        self.l2 = self.config["l2"]
+        self.dropout = self.config["dropout"]
+        self.res = self.config["res"]
+
+        self.conv1 = ConvBlock(
+            filters=self.num_filters,
+            kernel_size=self.filter_size,
+            pool_size=self.pool_size,
+            activation=self.activation,
+            l2=self.l2,
+            dropout=0.3,
+            res=False,
+        )
+        self.conv2a = ConvBlock(
+            filters=int(self.num_filters / 2),
+            kernel_size=3,
+            pool_size=2,
+            activation="relu",
+            l2=self.l2,
+            dropout=self.dropout,
+            res=self.res,
+        )
+        self.conv2b = ConvBlock(
+            filters=int(self.num_filters / 2),
+            kernel_size=3,
+            pool_size=2,
+            activation="relu",
+            l2=self.l2,
+            dropout=self.dropout,
+            res=self.res,
+        )
+        self.conv3a = ConvBlock(
+            filters=int(self.num_filters / 4),
+            kernel_size=3,
+            pool_size=2,
+            activation="relu",
+            l2=self.l2,
+            dropout=self.dropout,
+            res=self.res,
+        )
+        self.conv3b = ConvBlock(
+            filters=int(self.num_filters / 4),
+            kernel_size=3,
+            pool_size=2,
+            activation="relu",
+            l2=self.l2,
+            dropout=self.dropout,
+            res=self.res,
+        )
+        self.pooling = layers.GlobalAveragePooling1D()
+        self.dense = layers.Dense(
+            units=self.num_classes,
+            activation="linear",
+            use_bias=True,
+        )
+
+    def call(self, inputs, training=False):
+        x = self.conv1(inputs)
+        x = self.conv2a(x)
+        x = self.conv2b(x)
+        x = self.conv3a(x)
+        x = self.conv3b(x)
+        x = self.pooling(x)
+        x = self.dense(x)
+
+        return x
