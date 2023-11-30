ae7b86690257342b7cc79a8a505b37747e4272d0
Lukas Mahieu <lukas.mahieu@kuleuven.be>
2023-11-30 13:50:51 +0100

    Rename src folder

--- deeplocus/data/make_dataset.py
+++ deeplocus/data/make_dataset.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
+import argparse
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description='DeepLocus')
+    parser.add_argument('--input', type=str, default='data/raw',
+                        help='input directory')
+    parser.add_argument('--output', type=str, default='data/processed',
+                        help='output directory')
+    args = parser.parse_args()
+    return args
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+
+def main(args):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
@@ -20,11 +27,9 @@ if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
+    # Load environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    args = parse_args()
+    main(args)
