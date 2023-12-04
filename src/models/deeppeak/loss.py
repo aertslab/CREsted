c4e5c03517b6d68c6e2ab25a0700bc77d9d0ad8a
lukasmahieu <lukas.mahieu@kuleuven.be>
2023-12-04 14:03:11 +0100

    loss function to class

--- src/models/deeppeak/loss.py
+++ src/models/deeppeak/loss.py
@@ -1,16 +1,19 @@
 """DeepPeak model loss functions."""
 import tensorflow as tf
-from tensorflow.python.ops import nn
-from tensorflow.python.ops import math_ops
 from tensorflow.python.keras import backend as K
+from tensorflow.keras.losses import Reduction
 
 
-@tf.function
-def custom_loss(y_true, y_pred):
-    y_true1 = nn.l2_normalize(y_true, axis=-1)
-    y_pred1 = nn.l2_normalize(y_pred, axis=-1)
-    y_pred2 = tf.convert_to_tensor(y_pred)
-    y_true2 = math_ops.cast(y_true, y_pred.dtype)
-    return -math_ops.reduce_sum(y_true1 * y_pred1, axis=-1) + K.mean(
-        math_ops.squared_difference(y_pred2, y_true2), axis=-1
-    )
+class CustomLoss(tf.keras.losses.Loss):
+    def __init__(self, reduction=Reduction.SUM, name="CustomLoss"):
+        super().__init__(reduction=reduction, name=name)
+
+    @tf.function
+    def call(self, y_true, y_pred):
+        y_true_normalized = tf.nn.l2_normalize(y_true, axis=-1)
+        y_pred_normalized = tf.nn.l2_normalize(y_pred, axis=-1)
+        cosine_loss = -tf.reduce_sum(y_true_normalized * y_pred_normalized, axis=-1)
+        squared_difference_loss = K.mean(
+            tf.math.squared_difference(y_pred, y_true), axis=-1
+        )
+        return cosine_loss + squared_difference_loss
