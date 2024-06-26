c4acbf5e8e9347f6f37cd84d3cff047c69f276ab
nkempynck <niklas.ke@hotmail.com>
2024-06-26 17:17:08 +0200

    added first part of pattern analysis (atac only)

--- src/crested/tl/__init__.py
+++ src/crested/tl/__init__.py
@@ -1,4 +1,4 @@
 from . import data, losses, metrics, zoo
 from ._configs import TaskConfig, default_configs
 from ._crested import Crested
-from ._tfmodisco import tfmodisco
+from ._tfmodisco import tfmodisco, match_h5_files_to_classes, process_patterns, create_pattern_matrix, generate_nucleotide_sequences
