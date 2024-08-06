066fd2ab8fa1c0ae7cf49abeb672b5fe1bea5b7f
lukasmahieu <lukas.mahieu@kuleuven.be>
2024-08-06 15:56:31 +0200

    more options for plotting

--- src/crested/pl/_utils.py
+++ src/crested/pl/_utils.py
@@ -14,7 +14,13 @@ def render_plot(
     title: str | None = None,
     xlabel: str | None = None,
     ylabel: str | None = None,
+    title_fontsize: int = 16,
+    x_label_fontsize: int = 14,
+    y_label_fontsize: int = 14,
+    x_tick_fontsize: int = 12,
+    y_tick_fontsize: int = 12,
     x_label_rotation: int = 0,
+    y_label_rotation: int = 0,
     show: bool = False,
     save_path: str | None = None,
 ) -> plt.Figure:
@@ -39,8 +45,20 @@ def render_plot(
         Label for the X-axis.
     ylabel
         Label for the Y-axis.
+    title_fontsize
+        Font size for the title.
+    x_label_fontsize
+        Font size for the X-axis labels.
+    y_label_fontsize
+        Font size for the Y-axis labels.
+    x_tick_fontsize
+        Font size for the X-axis ticks.
+    y_tick_fontsize
+        Font size for the Y-axis ticks
     x_label_rotation
         Rotation of the X-axis labels in degrees.
+    y_label_rotation
+        Rotation of the Y-axis labels in degrees.
     show
         Whether to display the plot.
     save_path
@@ -48,14 +66,18 @@ def render_plot(
     """
     fig.set_size_inches(width, height)
     if title:
-        fig.suptitle(title)
+        fig.suptitle(title, fontsize=title_fontsize)
     for ax in fig.axes:
         if xlabel:
-            ax.set_xlabel(xlabel)
+            ax.set_xlabel(xlabel, fontsize=x_label_fontsize)
         if ylabel:
-            ax.set_ylabel(ylabel)
+            ax.set_ylabel(ylabel, fontsize=y_label_fontsize)
         for label in ax.get_xticklabels():
             label.set_rotation(x_label_rotation)
+            label.set_fontsize(x_tick_fontsize)
+        for label in ax.get_yticklabels():
+            label.set_fontsize(y_tick_fontsize)
+            label.set_rotation(y_label_rotation)
 
     plt.tight_layout()
     if save_path:
