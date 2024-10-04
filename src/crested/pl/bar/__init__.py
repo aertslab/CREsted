bac1af6620875447fe2b52aba1ec5e91684d309e
LukasMahieu <lukas.mahieu@kuleuven.be>
2024-10-04 10:15:56 +0200

    fix import

--- src/crested/pl/bar/__init__.py
+++ src/crested/pl/bar/__init__.py
@@ -1,2 +1,2 @@
 from ._normalization_weights import normalization_weights
-from ._region import prediction_bar, region, region_predictions
+from ._region import prediction, region, region_predictions
