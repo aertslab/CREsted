d3229fe9f8c4161e00609eda62d1f6e8b1a2b6af
LukasMahieu <lukas.mahieu@gmail.com>
2024-12-01 15:26:20 +0100

    unit tests and handling of path chromsizes

--- src/crested/_genome.py
+++ src/crested/_genome.py
@@ -18,7 +18,7 @@ class Genome:
     fasta
         The path to the FASTA file.
     chrom_sizes
-        A dictionary containing chromosome names and sizes.
+        A path to a tab delimited chromsizes file or a dictionary containing chromosome names and sizes.
     annotation
         The path to the annotation file.
     """
@@ -27,65 +27,71 @@ class Genome:
         self,
         *,
         fasta: Path,
-        chrom_sizes: dict[str, int] | None = None,
+        chrom_sizes: dict[str, int] | Path | None = None,
         annotation: Path | None = None,
     ):
         """Initialize the Genome object."""
         if isinstance(fasta, Path) or isinstance(fasta, str):
             self._fasta = Path(fasta)
-            self._fetch_fasta = None
         else:
             raise ValueError("fasta must be a Path.")
         self._annotation = annotation
-        self._chrom_sizes = chrom_sizes
+        if isinstance(chrom_sizes, str) or isinstance(chrom_sizes, Path):
+            self._chrom_sizes = Path(chrom_sizes)
+        else:
+            self._chrom_sizes = chrom_sizes
 
     @property
-    def fasta(self):
+    def fasta(self) -> Path:
         """
         The Path to the FASTA file.
 
         Returns
         -------
-        Path
-            The path to the FASTA file.
+        The path to the FASTA file.
         """
         return self._fasta
 
     @property
-    def annotation(self):
+    def annotation(self) -> Path | None:
         """
         The Path to the annotation file.
 
         Returns
         -------
-        Path
-            The path to the annotation file.
+        The path to the annotation file.
         """
         return self._annotation
 
     @property
-    def chrom_sizes(self):
+    def chrom_sizes(self) -> dict[str, int]:
         """
-        A dictionary with chromosome names as keys and their lengths as values.
+        A dictionary with chromosome names as keys and their lengths as valeus.
 
         Returns
         -------
-        dict[str, int]
-            A dictionary of chromosome sizes.
+        A dictionary of chromosome sizes.
         """
         if self._chrom_sizes is None:
             from pysam import FastaFile
 
             fasta = FastaFile(self.fasta)
             self._chrom_sizes = dict(zip(fasta.references, fasta.lengths))
+        elif isinstance(self._chrom_sizes, Path):
+            from crested._io import _read_chromsizes
+
+            self._chrom_sizes = _read_chromsizes(self._chrom_sizes)
+        elif not isinstance(self._chrom_sizes, dict):
+            print(self._chrom_sizes)
+            raise ValueError("chrom_sizes must be a dictionary or a Path.")
         return self._chrom_sizes
 
 
 def register_genome(genome: Genome):
     """
-    Register a genome to be used throughout the package.
+    Register a genome to be used throughout a session.
 
-    Once a genome is registered, all the functions in the package that require a genome can access it.
+    Once a genome is registered, all the functions in the package that require a genome will use it if not explicitly provided.
 
     Parameters
     ----------
---
+++ tests/test_data.py
@@ -0,0 +1,107 @@
+"""Pytest for data module."""
+
+from contextlib import contextmanager
+
+import pytest
+from loguru import logger
+
+
+@contextmanager
+def log_capture(level="WARNING"):
+    messages = []
+    handler_id = logger.add(messages.append, level=level)
+    try:
+        yield messages
+    finally:
+        logger.remove(handler_id)
+
+
+def test_genome_persistence():
+    """Test that the genome object is correctly stored."""
+    import crested
+
+    # check that does not yet exist
+    assert crested._conf.genome is None
+
+    genome = crested.Genome(
+        fasta="tests/data/test.fasta",
+        chrom_sizes={"chr1": 1000, "chr2": 2000},
+        annotation="tests/data/test.gtf",
+    )
+    crested.register_genome(genome)
+
+    # check that it is stored
+    assert crested._conf.genome == genome
+
+
+def test_no_genome_fasta():
+    """Test that a TypeError is raised if no fasta is provided."""
+    import crested
+
+    with pytest.raises(TypeError):
+        crested.Genome(
+            chrom_sizes={"chr1": 1000, "chr2": 2000}, annotation="tests/data/test.gtf"
+        )
+
+
+def test_import_beds_with_genome():
+    """Test that import_beds uses genome chromsizes."""
+    import crested
+
+    # Scenario 1: Genome registered with chromsizes provided
+    with log_capture(level="WARNING") as messages:
+        genome = crested.Genome(
+            fasta="tests/data/test.fasta",
+            chrom_sizes="tests/data/test.chrom.sizes",
+            annotation="tests/data/test.gtf",
+        )
+        crested.register_genome(genome)
+        crested.import_beds(
+            beds_folder="tests/data/test_topics",
+            regions_file="tests/data/test.regions.bed",
+        )
+
+    warning_messages = list(messages)
+    warning_text_chromsizes = (
+        "Chromsizes file not provided. Will not check if regions are within chromosomes"
+    )
+    warning_text_filtered = "Filtered 1 consensus regions (not within chromosomes)"
+    assert all(
+        warning_text_chromsizes not in msg for msg in warning_messages
+    ), "Warning about missing chromsizes was unexpectedly raised."
+    assert any(
+        warning_text_filtered in msg for msg in warning_messages
+    ), "Expected warning about filtered regions was not raised."
+
+    # Scenario 2: Chromsizes provided via parameter, no genome registered
+    crested._conf.genome = None
+    with log_capture(level="WARNING") as messages:
+        crested.import_beds(
+            beds_folder="tests/data/test_topics",
+            regions_file="tests/data/test.regions.bed",
+            chromsizes_file="tests/data/test.chrom.sizes",
+        )
+
+    warning_messages = list(messages)
+    assert all(
+        warning_text_chromsizes not in msg for msg in warning_messages
+    ), "Warning about missing chromsizes was unexpectedly raised."
+    assert any(
+        warning_text_filtered in msg for msg in warning_messages
+    ), "Expected warning about filtered regions was not raised."
+
+    # Scenario 3: No chromsizes provided via genome or parameter
+    with log_capture(level="WARNING") as messages:
+        crested.import_beds(
+            beds_folder="tests/data/test_topics",
+            regions_file="tests/data/test.regions.bed",
+            chromsizes_file=None,  # Explicitly set to None
+        )
+
+    warning_messages = list(messages)
+    assert any(
+        warning_text_chromsizes in msg for msg in warning_messages
+    ), "Expected warning about missing chromsizes was not raised."
+    assert all(
+        warning_text_filtered not in msg for msg in warning_messages
+    ), "Warning about filtered regions was unexpectedly raised."
