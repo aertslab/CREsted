5da337a1716ab670e726f9fc59efe7bf13a31bd7
casblaauw <38132585+casblaauw@users.noreply.github.com>
2025-03-19 18:39:31 +0100

    Add locus folder to crested.pl.__init__

--- src/crested/pl/__init__.py
+++ src/crested/pl/__init__.py
@@ -1,4 +1,4 @@
 """Init file for the pl module."""
 
-from . import bar, heatmap, hist, patterns, scatter
+from . import bar, heatmap, hist, locus, patterns, scatter
 from ._utils import render_plot
