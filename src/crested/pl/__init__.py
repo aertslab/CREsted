c086a67907f085335b1228015503abbce86a8fcf
casblaauw <38132585+casblaauw@users.noreply.github.com>
2026-02-10 00:20:16 +0100

    Simplify pl.modisco importing

--- src/crested/pl/__init__.py
+++ src/crested/pl/__init__.py
@@ -1,5 +1,53 @@
 """Init file for the pl module."""
 
+from importlib.util import find_spec
+
+from loguru import logger
+
 from . import corr, design, dist, explain, locus, modisco, qc, region
 from ._old import bar, heatmap, hist, patterns, scatter, violin
 from ._utils import create_plot, render_plot
+
+if find_spec("modiscolite") is not None:
+    MODISCOLITE_AVAILABLE = True
+else:
+    MODISCOLITE_AVAILABLE = False
+
+if MODISCOLITE_AVAILABLE:
+    try:
+        import modiscolite
+
+        from . import modisco
+    except ImportError as e:
+        logger.error(f"Import error: {e}")
+        raise
+else:
+    logger.warning(
+        "modiscolite is not installed, 'crested.pl.modisco' module will not be available. "
+        "Install with: pip install crested[motif]"
+    )
+
+__all__ = [
+    # Submodules
+    "corr",
+    "design",
+    "dist",
+    "explain",
+    "locus",
+    "modisco",
+    "qc",
+    "region",
+    # Individual functions
+    "create_plot",
+    "render_plot",
+    # Old alias modules
+    "bar",
+    "heatmap",
+    "hist",
+    "patterns",
+    "scatter",
+    "violin"
+]
+
+if MODISCOLITE_AVAILABLE:
+    __all__.extend("modisco")
--- src/crested/pl/modisco/__init__.py
+++ src/crested/pl/modisco/__init__.py
@@ -1,65 +1,13 @@
 """Initialize the modisco module."""
 
-from importlib.util import find_spec
-
-from loguru import logger
-
-
-def _optional_function_warning(*args, **kwargs):
-    logger.error(
-        "The requested functionality requires the 'tfmodisco' package, which is not installed. "
-        "Please install it with `pip install modisco-lite>=2.2.1.",
-    )
-
-if find_spec("modiscolite") is not None:
-    MODISCOLITE_AVAILABLE = True
-else:
-    MODISCOLITE_AVAILABLE = False
-
-if MODISCOLITE_AVAILABLE:
-    try:
-        import modiscolite
-
-        # Import all necessary functions from _modisco_results
-        from ._modisco import (
-            class_instances,
-            clustermap,
-            clustermap_tf_motif,
-            clustermap_tomtom_similarities,
-            clustermap_with_pwm_logos,
-            modisco_results,
-            selected_instances,
-            similarity_heatmap,
-            tf_expression_per_cell_type,
-        )
-    except ImportError as e:
-        logger.error(f"Import error: {e}")
-        raise
-else:
-    clustermap = _optional_function_warning
-    clustermap_tomtom_similarities = _optional_function_warning
-    modisco_results = _optional_function_warning
-    selected_instances = _optional_function_warning
-    similarity_heatmap = _optional_function_warning
-    class_instances = _optional_function_warning
-    clustermap_tf_motif = _optional_function_warning
-    tf_expression_per_cell_type = _optional_function_warning
-    clustermap_with_pwm_logos = _optional_function_warning
-
-# Export these functions for public use
-__all__ = []
-
-if MODISCOLITE_AVAILABLE:
-    __all__.extend(
-        [
-            "clustermap",
-            "modisco_results",
-            "class_instances",
-            "similarity_heatmap",
-            "selected_instances",
-            "clustermap_tf_motif",
-            "tf_expression_per_cell_type",
-            "clustermap_with_pwm_logos",
-            "clustermap_tomtom_similarities",
-        ]
+from ._modisco import (
+        class_instances,
+        clustermap,
+        clustermap_tf_motif,
+        clustermap_tomtom_similarities,
+        clustermap_with_pwm_logos,
+        modisco_results,
+        selected_instances,
+        similarity_heatmap,
+        tf_expression_per_cell_type,
     )
