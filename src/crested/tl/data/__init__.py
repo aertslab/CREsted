36a3538cc11c3531c7592d77ba180e641972e601
casblaauw <38132585+casblaauw@users.noreply.github.com>
2026-05-07 15:47:36 +0200

    Clean up code

--- src/crested/tl/data/__init__.py
+++ src/crested/tl/data/__init__.py
@@ -10,5 +10,3 @@ from ._old._anndatamodule import AnnDataModule
 from ._old._dataloader import AnnDataLoader
 from ._old._dataset import AnnDataset
 from ._sequenceloader import SequenceLoader
-from ._trackdata import TrackData
-from ._trackdatawrapper import GeckoDataWrapper, TrackDataWrapper
