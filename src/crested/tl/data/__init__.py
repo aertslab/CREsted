1a4d5dd1b8a1ef2a98545187d30e3fa81229a020
casblaauw <38132585+casblaauw@users.noreply.github.com>
2026-05-07 18:07:05 +0200

    Drop public imports of datamodule/wrapper internals like AnnDataset or BaseDataWrapper

--- src/crested/tl/data/__init__.py
+++ src/crested/tl/data/__init__.py
@@ -1,12 +1,8 @@
 """
 Utility functions to prepare data for training and evaluation.
 
-Generally, `tl.data.AnnDataModule` is the only one that should be called directly by the user.
+Generally, `tl.data.AnnDataWrapper` or `tl.data.AnnDataModule` is the only one that should be called directly by the user.
 """
 
 from ._anndatawrapper import AnnDataWrapper
-from ._datawrapper import BaseDataWrapper, BaseGenomicDataWrapper
 from ._old._anndatamodule import AnnDataModule
-from ._old._dataloader import AnnDataLoader
-from ._old._dataset import AnnDataset
-from ._sequenceloader import SequenceLoader
